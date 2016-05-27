3853b56365a65e59f379143e06868bc105b9c2f3
Egor Vinogradov <egorvinogradov@live.ru>
2016-05-27 13:47:57 -0700

    fix startup script

--- battery_watch_startup.sh
+++ battery_watch_startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-$dir=.battery_watch
+dir=".battery_watch"
 
 if [[ ! -e $dir ]]; then
   mkdir $dir
 fi
 
-sleepwatcher -s "echo "BATTERY_WATCH=0" > ~/.battery_watch/status" &
-sleepwatcher -w "echo "BATTERY_WATCH=1" > ~/.battery_watch/status" &
+sleepwatcher -s "echo 'BATTERY_WATCH=0' > ~/$dir/status" &
+sleepwatcher -w "echo 'BATTERY_WATCH=1' > ~/$dir/status" &
 
 node battery_watch.js &
